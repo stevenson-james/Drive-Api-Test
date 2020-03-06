b8b5ed5da1fca1f12f49f54b879d158cc689f407
James Stevenson <jpatstevenson@gmail.com>
2020-03-06 15:57:21 -0800

    working code

--- googleDrive.py
+++ googleDrive.py
@@ -6,22 +6,19 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 from googleapiclient.http import MediaFileUpload
 
-
-# If modifying these scopes, delete the file token.pickle.
 SCOPES = ['https://www.googleapis.com/auth/drive']
 
 def main():
-    """Shows basic usage of the Drive v3 API.
-    Prints the names and ids of the first 10 files the user has access to.
-    """
+
     creds = None
     # The file token.pickle stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
     # time.
+    # If you want to reset account, delete pickle file
     if os.path.exists('token.pickle'):
         with open('token.pickle', 'rb') as token:
             creds = pickle.load(token)
-    # If there are no (valid) credentials available, let the user log in.
+    # If there are no valid credentials available, let the user log in.
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
@@ -34,29 +31,38 @@ def main():
         with open('token.pickle', 'wb') as token:
             pickle.dump(creds, token)
 
+    #build the instance for api calls
     drive_service = build('drive', 'v3', credentials=creds)
-    
-    # Call the Drive v3 API
     if drive_service:
         print ('Successful connection')
-            
-    # Create a folder on Drive
+
+    # Create a folder for scare videos on drive account
     folderName = 'Scare Videos'
-    
-    #TODO: how to check if folder already exists
-    #response = drive_service.files().list(q="mimeType='application/vnd.google-apps.folder', name = " + folderName)
-    file_metadata = {
-      'name': folderName,
-      'mimeType': 'application/vnd.google-apps.folder'
-    }
-    root_folder = drive_service.files().create(body = file_metadata).execute()
-    print('Folder \'' + folderName + '\' created')
+
+
+    results = drive_service.files().list(
+        q="mimeType=\'application/vnd.google-apps.folder\' and name = \'" + folderName + "\' and trashed = false",
+        pageSize = 1,
+        fields = 'files(id, name)').execute()
+    if not results.get('files', []): 
+        file_metadata = {
+        'name': folderName,
+        'mimeType': 'application/vnd.google-apps.folder'
+        }
+        root_folder = drive_service.files().create(body = file_metadata).execute()
+        print('Folder \'%s\' created' % folderName)
 
     #upload scare cam video
-    file_metadata = {'name': 'scare_02-21 22-36-22.h264'}
-    media = MediaFileUpload('scare_02-21 22-36-22.h264', mimetype='video/h264')
-    file = drive_service.files().create(body=file_metadata, media_body=media, fields='id').execute()
-    print ('File ID: %s' % file.get('id'))
+    fileName = 'scare_02-21 22-36-22.h264'
+    results = drive_service.files().list(
+        q="mimeType=\'video/h264\' and name = \'" + fileName + "\' and trashed = false",
+        pageSize = 1,
+        fields = 'files(id, name)').execute()
+    if not results.get('files', []):
+        file_metadata = {'name': fileName}
+        media = MediaFileUpload(fileName, mimetype='video/h264')
+        file = drive_service.files().create(body=file_metadata, media_body=media, fields='id, name').execute()
+        print ('File \'%s\' added' % file.get('name'))
 
 if __name__ == '__main__':
     main()
